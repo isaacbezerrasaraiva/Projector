bf4ff09ff31fda955a0554de69f41d2e2fcbb9ab
Isaac Saraiva <isaacbezerrasaraiva@outlook.com>
2021-03-17 18:40:47 -0300

    1. Project structure definitions 2. Added extra partial class to MainWindow handle specific code portions

---
+++ v1.0.0.0/Modules/Projector/Source/Projector/MainWindow/MainWindowPlaylist.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Projector
+{
+    public partial class MainWindow : Window
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
